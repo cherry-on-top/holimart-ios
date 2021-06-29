0adff883786712cf54f6bb6190d8d184318c9c46
foxwavez <foxwavez@gmail.com>
2021-06-30 00:14:05 +0900

    Initial Commit

---
+++ holimart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  holimart
+//
+//  Created by Hailey Lee on 2021/06/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
