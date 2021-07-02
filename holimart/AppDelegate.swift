060d78e4dbbbc25dff7fc1e369b9c71bdc3ffce6
foxwavez <foxwavez@gmail.com>
2021-07-02 15:42:43 +0900

    SceneDelegate 삭제 -> info.plist 까지,  앱델리게이트 루트뷰 설정

--- holimart/AppDelegate.swift
+++ holimart/AppDelegate.swift
@@ -10,11 +10,20 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        setUpRootViewController()
         return true
+    }
+    
+    private func setUpRootViewController() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let viewController = ViewController()
+        
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
     }
 
     // MARK: UISceneSession Lifecycle
