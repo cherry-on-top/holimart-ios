e698657949d77253e5513999e1588df6b8681b25
foxwavez <foxwavez@gmail.com>
2021-07-06 00:20:31 +0900

    위젯 스펠링 틀린 거 수정, 탭바에 위젯뷰 추가
    이동 가능하게 만들어야 함 -> 아직 구현 미비

--- holimart/ViewController.swift
+++ holimart/ViewController.swift
@@ -16,18 +16,22 @@ class ViewController: UITabBarController {
     }
     
     
-    func setUpTabBar() {
+    private func setUpTabBar() {
         tabBar.tintColor = .black
         tabBar.backgroundColor = .white
         
         let homeViewController = HomeViewController()
+        let widgetViewController = WidgetViewController()
+        
         
         let unselectedImage = UIImage(systemName: "house")
         let selectedImage = UIImage(systemName: "house.fill")
         
         homeViewController.tabBarItem = UITabBarItem(title: "홈", image: unselectedImage, selectedImage: selectedImage)
         
-        let viewControllers = [homeViewController]
+        widgetViewController.tabBarItem = UITabBarItem(title: "위젯", image: UIImage(systemName: "square.grid.2x2"), selectedImage: UIImage(systemName: "square.grid.2x2.fill"))
+        
+        let viewControllers = [homeViewController, widgetViewController]
         
         self.viewControllers = viewControllers
     }
---
+++ holimart/WidgetViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WedgetViewController.swift
+//  holimart
+//
+//  Created by Hailey Lee on 2021/07/05.
+//
+
+import UIKit
+
+class WidgetViewController: UIViewController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBarController?.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
