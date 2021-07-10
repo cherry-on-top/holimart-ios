6ab09ee2e9bcdc2ab8a9c7494187a9f54c2b7273
foxwavez <foxwavez@gmail.com>
2021-07-10 21:24:11 +0900

    카카오맵 sdk는  ObjectC로 되어 있어서, 스위프트 언어를 쓰기 위해 추가

---
+++ holimart-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  holimart-Bridging-Header.h
+//  holimart
+//
+//  Created by Hailey Lee on 2021/07/10.
+//
+
+#ifndef holimart_Bridging_Header_h
+#define holimart_Bridging_Header_h
+
+#import <DaumMap/MTMapView.h>
+
+#endif /* holimart_Bridging_Header_h */
