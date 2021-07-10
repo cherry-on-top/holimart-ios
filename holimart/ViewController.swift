6cee907f030ea8db108c4963f86c5ecb4c51027a
foxwavez <foxwavez@gmail.com>
2021-07-10 21:24:34 +0900

    일단 기본 뷰에 카카오맵 띄우기 성공

--- holimart/ViewController.swift
+++ holimart/ViewController.swift
@@ -7,12 +7,21 @@
 
 import UIKit
 
-class ViewController: UITabBarController {
+class ViewController: UITabBarController, MTMapViewDelegate {
+    
+    var mapView: MTMapView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpTabBar()
         view.backgroundColor = .black
+        
+        mapView = MTMapView(frame: self.view.bounds)
+        if let mapView = mapView {
+            mapView.delegate = self
+            mapView.baseMapType = .standard
+            self.view.addSubview(mapView)
+        }
     }
     
     
