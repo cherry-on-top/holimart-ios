17f7a5e7a6f8910b1dab29b192d7e84908600a16
foxwavez <foxwavez@gmail.com>
2021-07-11 22:59:20 +0900

    카카오맵 지도 중심점, 현재 위치 트래킹 완료, 마커 에러남

--- holimart/HomeViewController.swift
+++ holimart/HomeViewController.swift
@@ -7,21 +7,60 @@
 
 import UIKit
 
+public let DEFAULT_POSITION = MTMapPointGeo(latitude: 37.576568, longitude: 127.029148)
+
 class HomeViewController: UIViewController, UITabBarControllerDelegate, MTMapViewDelegate  {
     
     var mapView: MTMapView?
+    
+    var mapPoint1: MTMapPoint?
+    var poiItem1: MTMapPOIItem?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBarController?.delegate = self
 //        view.backgroundColor = .blue
         
+        
+        // 지도 불러오기
         mapView = MTMapView(frame: self.view.bounds)
+        
         if let mapView = mapView {
             mapView.delegate = self
             mapView.baseMapType = .standard
+            
+            // 지도 중심점, 레벨
+            mapView.setMapCenter(MTMapPoint(geoCoord: DEFAULT_POSITION ), zoomLevel: 2, animated: true)
+                        
+            // 현재 위치 트래킹
+            mapView.showCurrentLocationMarker = true
+            mapView.currentLocationTrackingMode = .onWithoutHeading
+                        
+            // 마커 추가
+            self.mapPoint1 = MTMapPoint(geoCoord: MTMapPointGeo(latitude:  37.587568, longitude: 127.029148))
+            poiItem1 = MTMapPOIItem()
+            poiItem1?.markerType = MTMapPOIItemMarkerType.yellowPin
+            poiItem1?.mapPoint = mapPoint1
+            poiItem1?.itemName = "제발 좀 나와주세요 마커야 보고 싶다"
+            mapView.add(poiItem1)
+                        
+//            mapView.addPOIItems([poiItem1])
+//            mapView.fitAreaToShowAllPOIItems()
+            
             self.view.addSubview(mapView)
         }
+    }
+    
+    // Custom: 현 위치 트래킹 함수
+    func mapView(_ mapView: MTMapView!, updateCurrentLocation location: MTMapPoint!, withAccuracy accuracy: MTMapLocationAccuracy) {
+        let currentLocation = location?.mapPointGeo()
+        if let latitude = currentLocation?.latitude, let longitude = currentLocation?.longitude{
+            print("MTMapView updateCurrentLocation (\(latitude),\(longitude)) accuracy (\(accuracy))")
+        }
+    }
+    
+    func mapView(_ mapView: MTMapView?, updateDeviceHeading headingAngle: MTMapRotationAngle) {
+        print("MTMapView updateDeviceHeading (\(headingAngle)) degrees")
     }
     
 
