530eac248226543baf49843ea9302423d973c536
foxwavez <foxwavez@gmail.com>
2021-07-19 17:01:55 +0900

    마커 추가, 실제 아이폰에서 빌드 확인

--- holimart/MapViewController.swift
+++ holimart/MapViewController.swift
@@ -9,34 +9,33 @@ import UIKit
 import CoreLocation
 import NMapsMap
 
-class MapViewController: UIViewController, UITabBarControllerDelegate {
+class MapViewController: UIViewController, UITabBarControllerDelegate, CLLocationManagerDelegate {
 
     var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        locationManager.stopUpdatingLocation()
         
         let locationStatus = CLLocationManager.authorizationStatus()
         print("위치 정보 권한 요청 밸류:",locationStatus.rawValue)
         
         let mapView = NMFMapView(frame: view.frame)
            view.addSubview(mapView)
-        mapView.positionMode = .compass
+        mapView.positionMode = .direction
         mapView.showLegalNotice()
         
+        let coord = NMGLatLng(lat: 37.5670135, lng: 126.9783740)
+
+        print("위도: \(coord.lat), 경도: \(coord.lng)")
     
+        let maker = NMFMarker()
+        maker.position = NMGLatLng(lat: 37.5670135, lng: 126.9783740)
+        maker.mapView = mapView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
