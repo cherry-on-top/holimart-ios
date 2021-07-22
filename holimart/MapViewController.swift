fce871c01bb45f4b6dbc853fbd390961da8ad7a3
foxwavez <foxwavez@gmail.com>
2021-07-23 01:06:48 +0900

    디바이스 빌드 - 위치 정보 권한 상태 확인
    CLLocatiion: 바로 쓰는 거 디프렉 됨, 인스턴스로 써줘야 함

--- holimart/MapViewController.swift
+++ holimart/MapViewController.swift
@@ -14,6 +14,8 @@ class MapViewController: UIViewController, UITabBarControllerDelegate, CLLocatio
 
     var locationManager = CLLocationManager()
     
+    var location = CLLocation()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,13 +24,15 @@ class MapViewController: UIViewController, UITabBarControllerDelegate, CLLocatio
         locationManager.startUpdatingLocation()
         locationManager.stopUpdatingLocation()
         
-        let locationStatus = CLLocationManager.authorizationStatus()
+        let locationStatus = locationManager.authorizationStatus
         print("위치 정보 권한 요청 밸류:",locationStatus.rawValue)
         
         let mapView = NMFMapView(frame: view.frame)
            view.addSubview(mapView)
         mapView.positionMode = .direction
         mapView.showLegalNotice()
+        
+        currentLocation(locationManager, didUpdateLocations: [location])
         
         let coord = NMGLatLng(lat: 37.5670135, lng: 126.9783740)
 
@@ -37,6 +41,32 @@ class MapViewController: UIViewController, UITabBarControllerDelegate, CLLocatio
         let maker = NMFMarker()
         maker.position = NMGLatLng(lat: 37.5670135, lng: 126.9783740)
         maker.mapView = mapView
+        
+        requestGPSPermission()
     }
     
+    func requestGPSPermission(){
+            
+        switch locationManager.authorizationStatus {
+        case .authorizedAlways, .authorizedWhenInUse:
+                print("GPS: 권한 있음")
+            case .restricted, .notDetermined:
+                print("GPS: 아직 선택하지 않음")
+            case .denied:
+                print("GPS: 권한 없음")
+            default:
+                print("GPS: Default")
+            }
+        }
+    
+    func currentLocation(
+        _ manager: CLLocationManager,
+        didUpdateLocations locations: [CLLocation]
+    ) {
+        if let location = locations.last {
+            let latitude = location.coordinate.latitude
+            let longitude = location.coordinate.longitude
+            // Handle location update
+        }
+    }
 }
