3d3a0f153afa4a5bce904898a32fdc56f2e8f600
foxwavez <foxwavez@gmail.com>
2021-07-30 01:14:14 +0900

    welcomeLabel 오토레이아웃 잡아주기 완료

--- holimart/BrandViewController.swift
+++ holimart/BrandViewController.swift
@@ -10,12 +10,35 @@ import Alamofire
 
 class BrandViewController: UIViewController {
 
+    
+    var welcomeLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemTeal
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        setUpUI()
+        setupConstraints()
     }
     
+    private func setUpUI() {
+        welcomeLabel.text = "HolliMart에 오신 것을 환영합니다."
+        welcomeLabel.font = UIFont(name: ".AppleSystemUIFont", size: 40)
+        welcomeLabel.textColor = .black
+        welcomeLabel.textAlignment = .left
+        welcomeLabel.numberOfLines = 0
+        view.addSubview(welcomeLabel)
+    }
+    
+    private func setupConstraints() {
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            welcomeLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
+            welcomeLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            welcomeLabel.widthAnchor.constraint(equalToConstant: 200),
+            welcomeLabel.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
 
     /*
     // MARK: - Navigation
