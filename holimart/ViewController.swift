0d2772bf374460d8914693a998e821141395456a
foxwavez <foxwavez@gmail.com>
2021-08-14 23:27:08 +0900

    새 프로젝트 생성 -> 기존 레포지 연결

--- holimart/ViewController.swift
+++ holimart/ViewController.swift
@@ -1,41 +1,19 @@
 //
 //  ViewController.swift
-//  holimart
+//  Holimart
 //
-//  Created by Hailey Lee on 2021/06/30.
+//  Created by Hailey Lee on 2021/08/14.
 //
 
 import UIKit
-import Alamofire
 
-class ViewController: UITabBarController {
-    
+class ViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUpTabBar()
-        view.backgroundColor = .black
-        
+        view.backgroundColor = .systemPink
     }
-    
-    
-    private func setUpTabBar() {
-        tabBar.tintColor = .black
-        tabBar.backgroundColor = .white
-        
-        let homeViewController = HomeViewController()
-        let widgetViewController = WidgetViewController()
-        let mapViewController = MapViewController()
-        
-        homeViewController.tabBarItem = UITabBarItem(title: "홈", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
-        
-        widgetViewController.tabBarItem = UITabBarItem(title: "위젯", image: UIImage(systemName: "square.grid.2x2"), selectedImage: UIImage(systemName: "square.grid.2x2.fill"))
-        
-        mapViewController.tabBarItem = UITabBarItem(title: "지도", image: UIImage(systemName: "map"), selectedImage: UIImage(systemName: "map.fill"))
-        
-        let viewControllers = [mapViewController, widgetViewController]
-        
-        self.viewControllers = viewControllers
-    }
-    
+
+
 }
 
